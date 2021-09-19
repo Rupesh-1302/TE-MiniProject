43ae0e0c59b82648b99ff1251006f21f9aeefd17
Rupesh Agarwal <rupeshagarwal955@gmail.com>
2021-09-19 19:47:33 +0530

    setting up react Router

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
   async function TestConnection() {
     const res = await axios.get("http://localhost:8000/home");
-    console.log(res.data);
+    setData(res.data);
   }
   useEffect(() => {
     TestConnection();
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/home">
+            <h1>{data}</h1>
+          </Route>
+          <Route path="/link">
+            <h1>This is link page</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
