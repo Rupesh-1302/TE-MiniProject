2e39d42a3fb6b1d0a7c4ce06b10bb5744751364f
Rupesh Agarwal <rupeshagarwal955@gmail.com>
2021-09-30 20:24:53 +0530

    material ui added and created navbar

--- Client/client/src/Components/User.js
+++ Client/client/src/Components/User.js
@@ -1,12 +1,10 @@
 import React from "react";
-import SideMenu from "./SideMenu";
 import TopNav from "./TopNav";
 
 function User() {
   return (
     <div>
       <TopNav />
-      <SideMenu />
     </div>
   );
 }
